77c5b1a17ed0944978b5adb3c3de7a74fb65c985
Lisa Scott <lscottcook@hotmail.com>
2018-09-17 09:19:20 -0400

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -2,3 +2,7 @@
 #filter using and/or operators
 df = df[((df['col1']=='apples') | (df['col2']=='grapes')) &
 (df['col4']=='female') | (df[col5]='pink')]
+
+#listing duplicates 
+match_columns = ['ColumnA', 'ColumnB']
+duplicates=  df.loc[df.duplicated(match_columns)]
